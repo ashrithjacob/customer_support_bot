8c2783b2197c3bee52bba3fc7f44e5083a1f9745
ash <ashrithjacob@gmail.com>
2024-12-10 21:33:49 +0530

    second update agent1

--- src/pages/article_generation.py
+++ src/pages/article_generation.py
@@ -8,12 +8,14 @@ def create_topics(row: dict) -> pd.DataFrame:
         "topic": None,
         "article": None,
     }
-    if row["status"] == "No Article Found" and row["topic"].lower() != "unclear":
+    if "No" in row["status"] and row["topic"].lower() != "unclear":
         article_row["no"]=row["no"]
         article_row["topic"]=row["topic"]
         article = helper.generate_article(row["description"], row["topic"])
         article_row["article"] = article
-    return article_row
+        return article_row
+    else:
+        return None
 
 
 
@@ -22,7 +24,9 @@ if st.button("Generate missing topics from previous table"):
     st.write("Creating topics....")
     table_placeholder = st.empty()
     for idx, row in st.session_state["table"].iterrows():
-        rows_new_table.append(create_topics(row))
+        row_new = create_topics(row)
+        if row_new is not None:
+            rows_new_table.append(row_new)
         table_formatted = pd.DataFrame(rows_new_table)
         with table_placeholder.container():
             st.table(table_formatted)
